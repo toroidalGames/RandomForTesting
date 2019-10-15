38f03f21198cf93623d90cbdf2077cdf3b514e6a
adam h <adamhigsonpt@outlook.com>
2019-10-15 14:54:25 +0100

    Created absorber and orc script, also changed player and orc prefabs

---
+++ Assets/Scripts/Absorber.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Absorber : MonoBehaviour
+{
+    [SerializeField] int takeOverPower = 15;
+    [SerializeField] bool takeOverMode = false;
+
+    IControllable controllableTarget = null;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && controllableTarget != null)
+        {
+            TakeOverCharacter();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IControllable collidedTarget = collision.gameObject.GetComponent<IControllable>();
+        if (collidedTarget != null)
+        {
+            controllableTarget = collidedTarget;
+        }
+    }
+
+    private void TakeOverCharacter()
+    {
+        controllableTarget.TakeOver(takeOverPower);
+    }
+}
---
+++ Assets/Scripts/Orc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class Orc : MonoBehaviour, IControllable
+{
+    [SerializeField] bool canBeTakenOver = true;
+    [SerializeField] int requiredTakeOverPower = 10;
+
+    public bool CanBeTakenOver()
+    {
+        return canBeTakenOver;
+    }
+
+    public void TakeOver(int takeOverPower)
+    {
+        if (takeOverPower > requiredTakeOverPower)
+        {
+            Debug.Log("You have enough power to take over: " + GetComponent<Character>().name);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
