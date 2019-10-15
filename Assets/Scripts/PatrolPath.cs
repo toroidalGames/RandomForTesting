581b74fd26ccfe985002dc00625f142460d2c3e8
adam h <adamhigsonpt@outlook.com>
2019-10-15 19:33:52 +0100

    Created a patrol path and pathpath.cs with code taken from rpg course

---
+++ Assets/Scripts/PatrolPath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+
+    const float WAY_POINT_GIZMO_RADIUS = 0.1f;
+
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            int j = GetNextIndex(i);
+
+            Gizmos.DrawSphere(GetWaypoint(i), WAY_POINT_GIZMO_RADIUS);
+            Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+        }
+    }
+    public int GetNextIndex(int i)
+    {
+        if (i + 1 < transform.childCount)
+        {
+            return i + 1;
+        }
+        else
+        {
+            return i = 0;
+        }
+    }
+
+    public Vector3 GetWaypoint(int i)
+    {
+        return transform.GetChild(i).position;
+    }
+}
