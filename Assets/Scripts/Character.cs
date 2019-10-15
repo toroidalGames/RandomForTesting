f5e97666fe65d211abfcbe739027e89233faedd8
adam h <adamhigsonpt@outlook.com>
2019-10-15 14:14:29 +0100

    Created character.cs

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    [SerializeField] string name;
+    [SerializeField] float hp = 100f;
+    [SerializeField] float attackDamage = 1f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
