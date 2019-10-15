cc25f13fa0860d3edb170ff841a8fcc41e10f7e8
adam h <adamhigsonpt@outlook.com>
2019-10-15 16:01:50 +0100

    Created animationChanger.cs so far works by making it so when player moving horitzontally it will change animation

---
+++ Assets/Scripts/AnimationChanger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationChanger : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void ChangeAnimationController(Animator animatorToChangeTo)
+    {
+        animator = animatorToChangeTo;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeToMovementAnimation();
+    }
+
+    private void ChangeToMovementAnimation()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            animator.SetBool("movingHorizontally", true);
+        }
+        else
+        {
+            animator.SetBool("movingHorizontally", false);
+        }
+
+    }
+}
